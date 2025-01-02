ccd1c11dfc3ee0f6315ef0fffe9c81be4d11704f
yatharth-singh148 <yatharthsingh.cs23@bmsce.ac.in>
2025-01-02 10:20:47 +0530

    Create middlelinkedlist.c

---
+++ middlelinkedlist.c
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/middle-of-the-linked-list/\
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* middleNode(struct ListNode* head) {
+    struct ListNode *slow = head, *fast = head;
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
